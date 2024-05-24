42eeb82948e2d310e77b8cc372bff6478544f81d
eduardoadf21 <eduardoadf.luis@gmail.com>
2024-05-24 18:51:20 -0300

    opção de sair da rede + tratamento de erros

--- peer.py
+++ peer.py
@@ -8,12 +8,12 @@ def createAndConnect(serverName,serverPort):
     clientSocket.connect((serverName,serverPort))
     return clientSocket
 
-menu = ('\nSelecione uma opção' +
-        '\na - Registrar-se como peer' +
+menu = ('\na - Registrar-se como peer' +
         '\nb - Registrar arquivo' +
         '\nc - Listar arquivos' +
         '\nd - Baixar arquivo' +
-        '\ne - Sair\n')
+        '\ne - Sair da rede'
+        '\nf - Sair\n')
 
 while True:
     print(menu)
@@ -26,21 +26,28 @@ while True:
         clientSocket.close()
     if (opcode == 'b'):
         nome_arquivo = input("Nome do Arquivo: ")
-        clientSocket.send(nome_arquivo.encode())
-        arquivo = clientSocket.recv(1024)
-        print(f'\narquivo {arquivo.decode()} registrado.')
-        clientSocket.close()
+        clientSocket.send(json.dumps(nome_arquivo).encode())
+        arquivo = json.loads(clientSocket.recv(1024))
+        if (arquivo == "403"):
+            print(f'\npeer não registrado na rede')
+            clientSocket.close()
+        else:
+            print(f'\narquivo {arquivo} registrado.')
+            clientSocket.close()
     if (opcode == 'c'):
         arquivos = json.loads(clientSocket.recv(1024))
         print(f'\narquivos: {arquivos}')
     if(opcode == 'd'):
         nome_arquivo = input("Nome do Arquivo: ")
-        clientSocket.send(nome_arquivo.encode())
-        answer = json.loads(clientSocket.recv(1024))
+        clientSocket.send(json.dumps(nome_arquivo).encode())
+        answer = json.loads(clientSocket.recv(1024).decode())
         if (answer == "404"):
             print(f'\narquivo não encontrado.')
         else:
             print(f'\narquivo encontrado no ip {answer}')
         clientSocket.close()
     if(opcode == 'e'):
+        print(f'\nsaiu da rede.')
+        clientSocket.close()
+    if(opcode == 'f'):
         break
